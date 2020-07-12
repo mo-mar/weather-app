68cea09e77a1fb0b7c8d8de7601f7707e72eea35
Mohamed Omar <mohamedsomar1@gmail.com>
2020-07-12 19:58:32 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import * as classes from "./App.module.css";
+
+async function fetchCityKey(cityName) {
+    let citiesMatchingString = await fetch(
+        `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=wHt3xF6xVQGmPslYZJCZ1bg7MhzYFsDp&q=${cityName}`
+    ).then((response) => response.json());
+    console.log(citiesMatchingString);
+}
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className={classes.App}>
+            <h1>Weather App</h1>
+            <input type="text" />
+            <button onClick={fetchCityKey}>BUTTON</button>
+        </div>
+    );
 }
 
 export default App;
