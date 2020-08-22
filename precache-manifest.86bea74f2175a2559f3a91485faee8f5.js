9371a23445a5b8e13c0cf486e6406e670609b773
Mohamed Omar <mohamedsomar1@gmail.com>
2020-08-22 18:03:10 -0400

    Updates

---
+++ precache-manifest.86bea74f2175a2559f3a91485faee8f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2ac575c93d4e1630420efcfe71e69d",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "d91b766f970a60969411",
+    "url": "/weather-app/static/css/main.12fb739a.chunk.css"
+  },
+  {
+    "revision": "d6f27d49c3ea91f7c6a3",
+    "url": "/weather-app/static/js/2.80d59011.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.80d59011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d91b766f970a60969411",
+    "url": "/weather-app/static/js/main.f0647be0.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
